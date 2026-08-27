2e1c420d17bb8e1840b025720dcd3c01c599d95f
funcnroll <115594042+funcnroll@users.noreply.github.com>
2026-08-27 15:27:24 +0200

    Implement NotFound

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import H1 from "@/components/ui/H1";
+
+function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center  min-h-screen">
+      <H1>404</H1>
+      <p>Page not found</p>
+    </div>
+  );
+}
+
+export default NotFound;
