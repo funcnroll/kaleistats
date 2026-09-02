b9f2897781ffe37773a9b65afcfd6ad20b1ae544
funcnroll <115594042+funcnroll@users.noreply.github.com>
2026-09-02 18:17:36 +0200

    Remove username plugin from BetterAuth

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,5 @@
 import { createAuthClient } from "better-auth/react";
-import { usernameClient } from "better-auth/client/plugins";
+
 import { nextCookies } from "better-auth/next-js";
 
 export const authClient = createAuthClient({
@@ -7,5 +7,5 @@ export const authClient = createAuthClient({
   refetchInterval: 60,
   refetchWhenOffline: false,
 
-  plugins: [usernameClient(), nextCookies()],
+  plugins: [nextCookies()],
 });
