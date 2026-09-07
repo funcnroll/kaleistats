830ee14a0bebd6657d52716bcda971ea1c865338
Mika <115594042+funcnroll@users.noreply.github.com>
2026-09-07 17:40:36 +0200

    Implement revocation of token via button

--- src/components/ui/TokenRow.tsx
+++ src/components/ui/TokenRow.tsx
@@ -1,3 +1,8 @@
+"use client";
+import { setTokenStatusDb } from "@/lib/setTokenStatusDb";
+import Button from "./Button";
+import { useRouter } from "next/navigation";
+
 function TokenRow({
   uuid,
   status,
@@ -7,13 +12,23 @@ function TokenRow({
   status: string;
   expireDate: string;
 }) {
+  const router = useRouter();
+
   return (
     <tr className="border-b border-neutral-800">
       <td>{uuid}</td>
       <td>{status}</td>
       <td>{expireDate}</td>
       <td>
-        <button>Action</button>
+        {/* May expand, but this suffices for now. */}
+        <Button
+          onClick={async () => {
+            await setTokenStatusDb("inactive", uuid);
+            router.refresh();
+          }}
+        >
+          Revoke
+        </Button>
       </td>
     </tr>
   );
