eacd82628a49ad18dd4881b074975184ab761109
Mika <115594042+funcnroll@users.noreply.github.com>
2026-09-07 17:24:31 +0200

    Implement dynamic row for each token

--- src/app/(auth)/dashboard/tokens/page.tsx
+++ src/app/(auth)/dashboard/tokens/page.tsx
@@ -1,10 +1,9 @@
 import TokenRow from "@/components/ui/TokenRow";
 import { getAllTokens } from "@/lib/getAllTokens";
+import { timestampToDate } from "@/lib/timestampToDate";
 
 async function Page() {
   const tokens = await getAllTokens();
-
-  console.log(tokens);
 
   return (
     <table className="w-full text-sm text-left text-neutral-200">
@@ -12,12 +11,21 @@ async function Page() {
         <tr className="border-b border-neutral-700">
           <th>Token</th>
           <th>Status</th>
-          <th>Expires</th>
+          <th>Expires (DD/MM/YYYY)</th>
           <th>Action</th>
         </tr>
       </thead>
       <tbody>
-        <TokenRow uuid="dfd" status="sds" expireDate="asdsa" />
+        {tokens.map((token) => {
+          return (
+            <TokenRow
+              uuid={token.tokenUUID}
+              status={token.status}
+              expireDate={timestampToDate(token.expireTime)}
+              key={token.tokenUUID}
+            />
+          );
+        })}
       </tbody>
     </table>
   );
