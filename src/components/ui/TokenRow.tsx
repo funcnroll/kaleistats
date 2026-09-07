bfe6c7066bc5c732288a64d700ad3649def53a53
Mika <115594042+funcnroll@users.noreply.github.com>
2026-09-07 17:26:19 +0200

    Properly type TokenRow

--- src/components/ui/TokenRow.tsx
+++ src/components/ui/TokenRow.tsx
@@ -1,4 +1,12 @@
-function TokenRow({ uuid, status, expireDate }) {
+function TokenRow({
+  uuid,
+  status,
+  expireDate,
+}: {
+  uuid: string;
+  status: string;
+  expireDate: string;
+}) {
   return (
     <tr className="border-b border-neutral-800">
       <td>{uuid}</td>
