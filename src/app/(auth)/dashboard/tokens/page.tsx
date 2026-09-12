87051a43b7f91334326e9863d143f308292f6772
Mika <115594042+funcnroll@users.noreply.github.com>
2026-09-12 08:14:17 +0200

    Implement filtering for tokens by status and UUID

--- src/app/(auth)/dashboard/tokens/page.tsx
+++ src/app/(auth)/dashboard/tokens/page.tsx
@@ -7,6 +7,8 @@ import TokenRow from "@/components/ui/TokenRow";
 import { getPaginatedTokens } from "@/lib/getPaginatedTokens";
 import { timestampToDate } from "@/lib/timestampToDate";
 import LastPaginationPageButton from "@/components/ui/LastPaginationPageButton";
+import SearchFilter from "@/components/ui/SearchFilter";
+import TokensPage from "@/components/pages/TokensPage";
 
 async function Page(props: {
   searchParams?: Promise<{
@@ -16,41 +18,7 @@ async function Page(props: {
   const currentPage = Number((await props.searchParams)?.page || 1);
   const currentTokens = await getPaginatedTokens(currentPage);
 
-  return (
-    <div className="w-full flex flex-col gap-8">
-      <TokenPageInput />
-      <div className="flex gap-4">
-        <LastPaginationPageButton currentPage={currentPage} />
-        <NextPaginationPageButton currentPage={currentPage} />
-        <NavButton path="/dashboard">Go back</NavButton>
-      </div>
-
-      {currentTokens.length > 0 ? (
-        <table className="text-sm text-left text-neutral-200">
-          <thead>
-            <tr className="border-b border-neutral-700">
-              <th>Token</th>
-              <th>Status</th>
-              <th>Expires (DD/MM/YYYY)</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {currentTokens.map((token) => (
-              <TokenRow
-                key={token.tokenUUID}
-                uuid={token.tokenUUID}
-                status={token.status}
-                expireDate={timestampToDate(token.expireTime)}
-              />
-            ))}
-          </tbody>
-        </table>
-      ) : (
-        <p>No tokens found.</p>
-      )}
-    </div>
-  );
+  return <TokensPage currentPage={currentPage} currentTokens={currentTokens} />;
 }
 
 export default Page;
